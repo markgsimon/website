b3d8ea6f4b7b8a0120f4365f7277794b97585828
Mark <mark.simon0917@gmail.com>
2022-06-13 10:44:36 -0400

    added fake header and email to website, let the games begin

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,12 +7,22 @@ import {
  Content
 } from "./styles"
 
+
+/**
+ * TODO
+ * Gather links and icons for:
+ *  github
+ *  linkedin
+ *  instagram
+ *  twitter
+ * 
+ */
 const Home = (props) => {
   return (
       <PageContainer>
-          <Header></Header>
+          <Header> This is where my navigation may go!!!</Header>
           <Content>Hello World! My name is Mark Simon!</Content>
-          <Footer></Footer>
+          <Footer> Look down here for contacts. For now I can be reached at mark.simon0917@gmail.com</Footer>
     </PageContainer>
   )
 }
