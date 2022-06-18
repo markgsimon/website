814a71f37cd3255effe6286059483ddae1593645
Mark <mark.simon0917@gmail.com>
2022-06-17 21:28:08 -0400

    added mutterings page basic description

--- src/pages/Mutterings/index.js
+++ src/pages/Mutterings/index.js
@@ -2,14 +2,21 @@ import React from 'react'
 
 
 import {
-
+  Text,
+  Body
 } from "./styles"
 import PageWrapper from '../PageWrapper';
 
 const Mutterings = (props) => {
   return (
     <PageWrapper>
-        <div style = {{color: "white"}}>Mutterings Page</div>
+      <Body>
+        <div style = {{color: "white", fontSize: "3em"}}>Mutterings Page</div>
+        <Text> This page will serve as the front page of my blog. Blogging is a super fun way to get your 
+          voice out there in the internet. I intend to use this as a way to express 
+          my love of learning through language as a guide for learning languages.
+        </Text>
+      </Body>
     </PageWrapper>
   )
 }
--- src/pages/Mutterings/styles.js
+++ src/pages/Mutterings/styles.js
@@ -1,1 +1,14 @@
 import styled from "styled-components";
+
+export const Text = styled.div`
+    color: antiquewhite;
+    font-family: sans-serif;
+    font-size: 2em;
+`;
+
+export const Body = styled.div` 
+
+    display: flex;
+    flex-direction: row;
+
+`;
