4aa2415d838716f87c7041ad8447ad65dd69d266
Mark <mark.simon0917@gmail.com>
2022-07-06 21:30:24 -0400

    added matts website to website

--- src/pages/Design/index.js
+++ src/pages/Design/index.js
@@ -17,6 +17,8 @@ const Design = (props) => {
         <Text> My first project is this website. 
           I hope someday to find like minded people around the globe to shre in my passions. 
           That is why I am proud to announce my first design project on this page. A blog. 
+
+          Newsflash: I began work helping a local photographer and good friend of mine, Matt Chafe. Check out his new <a href = "mattchafe.com">site</a>.
         </Text>
       </DesignContent>
     </PageWrapper>
