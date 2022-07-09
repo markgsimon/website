788e2df49a70373b2deca2678b807b5381b404e6
Mark <mark.simon0917@gmail.com>
2022-07-09 15:43:50 -0400

    fixed title

--- src/pages/Design/index.js
+++ src/pages/Design/index.js
@@ -15,7 +15,7 @@ const Design = (props) => {
       <DesignContent>
         <Title>Design page.</Title>
         <Text> My first project is this website. 
-          I hope someday to find like minded people around the globe to shre in my passions. 
+          I hope someday to find like minded people around the globe to share in my passions. 
           That is why I am proud to announce my first design project on this page. A blog. 
 
           Newsflash: I began work helping a local photographer and good friend of mine, Matt Chafe. Check out his new <a href = "http://www.mattchafe.com">site</a>.
